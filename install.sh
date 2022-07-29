acc205a1022ea621b52c3471e070ac8052588fe4
ChipCE <login721@gmail.com>
2022-07-29 09:40:18 +0900

    n/a

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 # move to script dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR
+cd "$DIR/src"
 # remove old config
 rm ~/klipper_config/*.cfg
 # copy new over
